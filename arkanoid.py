e8e991c7bf40be2b936dd1c41ba68b1602e9a26e
Emerald256 <133761254+Emerald256@users.noreply.github.com>
2024-01-08 19:58:28 +1000

    Add files via upload

---
+++ arkanoid.py
@@ -0,0 +1,135 @@
+import pygame
+import random
+
+
+FPS = 60
+SPEED = 5
+
+LEFT = 0
+RIGHT = 1
+NONE = -1
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (63, 72, 204)
+
+window_width = 1080
+window_height = 720
+flags = pygame.RESIZABLE
+background_color = WHITE
+
+platform_pos = []
+platform_width = 0
+platform_height = 0
+
+
+def main():
+    global screen
+    pygame.init()
+    window_size = (window_width, window_height)
+    screen = pygame.display.set_mode(window_size, flags)
+    pygame.display.set_caption("ZzeRoTiMe Project")
+    windowUpdate()
+    while True:
+        runArch(0)
+
+
+def windowUpdate():
+    screen.fill(background_color)
+    createPlatform()
+    pygame.display.update()
+
+
+def createPlatform():
+    global platform_pos, platform_width, platform_height
+    platform_width = window_width // 14
+    platform_height = 10
+    if len(platform_pos) == 0:
+        platform_x = window_width // 2 - platform_width // 2
+        platform_y = window_height - 50
+        platform_pos = [platform_x, platform_y]
+    pygame.draw.rect(screen, BLACK, (platform_pos[0], platform_pos[1], platform_width, platform_height))
+
+
+def showPauseText():
+    font_pause = pygame.font.Font(None, 72)
+    text_pause = font_pause.render("Пауза", False, BLACK)
+    text_width, text_height = font_pause.size("Пауза")
+    screen.blit(text_pause, (window_width // 2 - text_width // 2, window_height // 2 - text_height))
+
+    font_continue = pygame.font.Font(None, 36)
+    text_continue = font_continue.render("Нажмите пробел, чтобы продолжить", False, BLACK)
+    text_width, text_height = font_continue.size("Нажмите пробел, чтобы продолжить")
+    screen.blit(text_continue, (window_width // 2 - text_width // 2, window_height // 1.75))
+
+def showPauseScreen():
+    global window_width, window_height
+    pause = pygame.Surface((window_width, window_height), pygame.SRCALPHA)
+    pause.fill((63, 72, 204, 127))
+    showPauseText()
+    screen.blit(pause, (0, 0))
+    pygame.display.update()
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.VIDEORESIZE:
+                infoObject = pygame.display.Info()
+                window_width = infoObject.current_w
+                window_height = infoObject.current_h
+                windowUpdate()
+                showPauseScreen()
+                return
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_SPACE]:
+                    return
+
+
+def generateLevel():
+    level = list()
+    for i in range(random.randint(1, 5)):
+        for j in range(random.randint(1, 10)):
+            pass
+
+
+
+def runArch(speed):
+    global window_height, window_width, platform_pos
+    direction = NONE
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            keys = pygame.key.get_pressed()
+            if event.type == pygame.KEYDOWN:
+                if keys[pygame.K_a]:
+                    direction = LEFT
+                if keys[pygame.K_d]:
+                    direction = RIGHT
+                if keys[pygame.K_SPACE]:
+                    showPauseScreen()
+            if event.type == pygame.VIDEORESIZE:
+                infoObject = pygame.display.Info()
+                window_width = infoObject.current_w
+                window_height = infoObject.current_h
+                if direction == NONE:
+                    platform_pos[0] = window_width // 2 - platform_width // 2
+                    platform_pos[1] = window_height - 50
+                windowUpdate()
+        if platform_pos[0] <= 0 or platform_pos[0] + platform_width >= window_width:
+            direction = not(direction)
+        if direction == RIGHT:
+            speed = SPEED
+        if direction == LEFT:
+            speed = -SPEED
+        platform_pos[0] += speed
+        windowUpdate()
+        pygame.time.Clock().tick(FPS)
+
+
+if __name__ == '__main__':
+    main()
+
